279dd8ed71fd633a7d8d2b11696ce61853693722
lhnguyen99 <55450357+lhnguyen99@users.noreply.github.com>
2020-10-11 13:36:02 +0700

    Update Control_Door_with_sensor_rain.ino

--- Project/Control_Door_with_sensor_rain/Control_Door_with_sensor_rain.ino
+++ Project/Control_Door_with_sensor_rain/Control_Door_with_sensor_rain.ino
@@ -1,22 +1,21 @@
   #include <Servo.h>
-int rainSensor = 6;   // Khai báo chân A6 kết nối cảm biến mưa.
-  #define servoPin  9     // Khai báo chân sô 9 kết nối động cơ servo.
+int rainSensor = 6;   
+  #define servoPin  9     
 
 Servo myservo; 
   
 void setup() {
   // put your setup code here, to run once:
-    pinMode(rainSensor,INPUT);// Đặt chân cảm biến mưa là INPUT, vì tín hiệu sẽ được truyền đến cho Arduino
-  Serial.begin(9600);// Khởi động Serial ở baudrate 9600
+  pinMode(rainSensor,INPUT);
+  Serial.begin(9600);
   Serial.println("Da khoi dong xong");
-   myservo.attach(servoPin); // Cấu hình đối tượng servo với chân số 9.
-
+  myservo.attach(servoPin); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int value = digitalRead(rainSensor);//Đọc tín hiệu cảm biến mưa
-  if (value == HIGH) { // Cảm biến đang không mưa
+  int value = digitalRead(rainSensor);
+  if (value == HIGH) { 
      delay(1000);  
     Serial.println("Dang khong mua");
      myservo.write(180); 
@@ -25,5 +24,5 @@ void loop() {
     Serial.println("Dang mua");
     myservo.write(0); 
   }
-//  delay(200); // Đợi 1 tí cho lần kiểm tra tiếp theo. Bạn hãy tham khảo bài "Viết chương trình không dùng làm delay" trên Arduino.VN để kết hợp đoạn code này và cả chương trình của bạn
+//  delay(200); 
 }
