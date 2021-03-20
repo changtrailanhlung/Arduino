fbd36d799d92290f2ba7ac1deb1528507dc7067b
lhnguyen99 <lehongnguyen1999@gmail.com>
2021-03-20 23:20:42 +0700

    Update Alcohol_content_measurement.ino

--- Project/Alcohol_content_measurement/Alcohol_content_measurement.ino
+++ Project/Alcohol_content_measurement/Alcohol_content_measurement.ino
@@ -1,15 +1,26 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 int val=0;
 
 void setup(){
-
-Serial.begin(9600);
+  lcd.begin();
+  lcd.backlight();
+  Serial.begin(9600);
+  
 }
 
 void loop(){
+  val=analogRead(A0);
+  Serial.println(val);
 
-val=analogRead(A0);
-
-Serial.println(val);
-
-delay(100);
+  lcd.setCursor(0,0);
+  lcd.print("Alcohol");
+  lcd.setCursor(0,1);
+  lcd.print(val);
+  
+  delay(100);
 }
